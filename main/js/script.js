98b80acfb002473948c82a26a8a0aa1325b229f8
vivekgohel2004 <122967043+vivekgohel2004@users.noreply.github.com>
2023-02-02 23:59:12 +0530

    all files are uploaded

---
+++ main/js/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navigation.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>
+{
+    hamburger.classList.remove("active");
+    navigation.classList.remove("active");
+}))
+
